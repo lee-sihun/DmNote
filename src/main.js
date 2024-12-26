d702a0e6e2791e6abbd142e0f663b17302d18e74
lee-sihun <cnsa201119@gmail.com>
2024-12-26 20:35:51 +0900

    feat: 키뷰어 초기 테스트

---
+++ src/main.js
@@ -0,0 +1,81 @@
+const { app, BrowserWindow, screen } = require('electron/main')
+const path = require('node:path')
+const { GlobalKeyboardListener } = require('node-global-key-listener')
+
+const v = new GlobalKeyboardListener()
+
+function handleKeyPress(e) {
+  const key = e.name;
+  const state = e.state;
+
+  switch (key) {
+    case "Z":
+    case "X":
+    case "DOT":
+    case "FORWARD SLASH":
+      console.log(`Pressed: ${key} ${state}`);
+      BrowserWindow.getAllWindows()[0].webContents.send('keyState', { key, state });
+      break;
+  }
+
+}
+
+//  // 키보드 리스너 설정 - 디버깅용
+//  v.addListener(function (e) {
+//   console.log('키 입력 감지:', {
+//     name: e.name,           // 키 이름
+//     state: e.state,         // 상태 (UP/DOWN)
+//     rawKey: e.rawKey,       // raw 키코드
+//     vKey: e.vKey,          // 가상 키코드
+//     scanCode: e.scanCode,   // 스캔 코드
+//     modifiers: e.modifiers  // 수정자 키
+//   });
+// });
+
+// function createWindow() {
+//   const win = new BrowserWindow({
+//     width: 800,
+//     height: 600,
+//     autoHideMenuBar: true,
+//     transparent: true,
+//     webPreferences: {
+//       preload: path.join(__dirname, 'preload.js')
+//     }
+//   })
+
+//   win.loadFile(path.join(__dirname, 'index.html'))
+// }
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 400,
+    height: 100,
+    frame: false,
+    transparent: true,
+    alwaysOnTop: true,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: true,
+      contextIsolation: false
+    }
+  })
+
+  // 화면 크기 얻기
+  const primaryDisplay = screen.getPrimaryDisplay()
+  const { width, height } = primaryDisplay.workAreaSize
+
+  // 우하단 위치 계산 및 설정
+  win.setPosition(width - 400, height - 100)
+
+  win.loadFile(path.join(__dirname, 'index.html'))
+}
+
+app.whenReady().then(() => {
+  v.addListener(handleKeyPress)
+  createWindow()
+})
+
+app.on('window-all-closed', () => {
+  v.kill()
+  app.quit()
+})
