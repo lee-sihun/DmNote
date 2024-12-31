1e54ec7544c2cd85e59071ce2f1b6523cfa396ed
lee-sihun <cnsa201119@gmail.com>
2024-12-31 14:08:39 +0900

    feat: 컬러 팔레트 추가

--- src/renderer/components/Canvas.jsx
+++ src/renderer/components/Canvas.jsx
@@ -1,5 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import { useDraggable } from "@hooks/useDraggable";
+import Palette from "./Palette";
+import ResetIcon from "@assets/svgs/reset.svg";
+import PaletteIcon from "@assets/svgs/palette.svg";
 
 export default function Canvas() {
   return (
@@ -10,6 +13,9 @@ export default function Canvas() {
 }
 
 function Grid() {
+  const [palette, setPalette] = useState(false);
+  const [color, setColor] = useState("transparent");
+
   const initialPositions = [
     { dx: 280, dy: 130, width: 60 },
     { dx: 360, dy: 130, width: 60 },
@@ -33,11 +39,29 @@ function Grid() {
     };
   });
 
+  const handlePaletteClose = () => {
+    if (palette) setPalette(false);
+  }
+
   return (
-    <div className="grid relative w-full h-[320px] bg-[#393A3F] rounded-[6px]">
+    <div 
+      className="grid-bg relative w-full h-[320px] bg-[#393A3F] rounded-[6px]"
+      style={{ backgroundColor: color === "transparent" ? "#393A3F" : color }}
+      onClick={handlePaletteClose}
+    >
       {keys.map((props, index) => (
         <Key key={index} {...props} />
       ))}
+      <button 
+        className="absolute flex items-center justify-center w-[30px] h-[30px] bg-[#101216] rounded-[6px] bottom-[57px] left-[18px]"
+        onClick={() => setPalette(!palette)}
+      >
+        <object type="image/svg+xml" data={PaletteIcon} className="pointer-events-none w-[12px] h-[12px]"/>
+      </button>
+      <button className="absolute flex items-center justify-center w-[30px] h-[30px] bg-[#101216] rounded-[6px] bottom-[18px] left-[18px]">
+        <object type="image/svg+xml" data={ResetIcon} className="pointer-events-none w-[12.75px] h-[12px]"/>
+      </button>
+      {palette && <Palette color={color} onColorChange={setColor} />}
     </div>
   );
 }
