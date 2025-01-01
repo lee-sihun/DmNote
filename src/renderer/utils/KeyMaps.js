f00840a6bd6117d663a17c6fa2b3872ccfa5e06c
lee-sihun <cnsa201119@gmail.com>
2025-01-01 15:01:08 +0900

    feat: 키맵 추가

---
+++ src/renderer/utils/KeyMaps.js
@@ -0,0 +1,125 @@
+export const keyMaps = {
+  // 알파벳
+  KeyA: { browserKey: 'a', globalKey: 'A', displayName: 'A' },
+  KeyB: { browserKey: 'b', globalKey: 'B', displayName: 'B' },
+  KeyC: { browserKey: 'c', globalKey: 'C', displayName: 'C' },
+  KeyD: { browserKey: 'd', globalKey: 'D', displayName: 'D' },
+  KeyE: { browserKey: 'e', globalKey: 'E', displayName: 'E' },
+  KeyF: { browserKey: 'f', globalKey: 'F', displayName: 'F' },
+  KeyG: { browserKey: 'g', globalKey: 'G', displayName: 'G' },
+  KeyH: { browserKey: 'h', globalKey: 'H', displayName: 'H' },
+  KeyI: { browserKey: 'i', globalKey: 'I', displayName: 'I' },
+  KeyJ: { browserKey: 'j', globalKey: 'J', displayName: 'J' },
+  KeyK: { browserKey: 'k', globalKey: 'K', displayName: 'K' },
+  KeyL: { browserKey: 'l', globalKey: 'L', displayName: 'L' },
+  KeyM: { browserKey: 'm', globalKey: 'M', displayName: 'M' },
+  KeyN: { browserKey: 'n', globalKey: 'N', displayName: 'N' },
+  KeyO: { browserKey: 'o', globalKey: 'O', displayName: 'O' },
+  KeyP: { browserKey: 'p', globalKey: 'P', displayName: 'P' },
+  KeyQ: { browserKey: 'q', globalKey: 'Q', displayName: 'Q' },
+  KeyR: { browserKey: 'r', globalKey: 'R', displayName: 'R' },
+  KeyS: { browserKey: 's', globalKey: 'S', displayName: 'S' },
+  KeyT: { browserKey: 't', globalKey: 'T', displayName: 'T' },
+  KeyU: { browserKey: 'u', globalKey: 'U', displayName: 'U' },
+  KeyV: { browserKey: 'v', globalKey: 'V', displayName: 'V' },
+  KeyW: { browserKey: 'w', globalKey: 'W', displayName: 'W' },
+  KeyX: { browserKey: 'x', globalKey: 'X', displayName: 'X' },
+  KeyY: { browserKey: 'y', globalKey: 'Y', displayName: 'Y' },
+  KeyZ: { browserKey: 'z', globalKey: 'Z', displayName: 'Z' },
+
+  // 숫자
+  Digit0: { browserKey: '0', globalKey: 'NUM 0', displayName: '0' },
+  Digit1: { browserKey: '1', globalKey: 'NUM 1', displayName: '1' },
+  Digit2: { browserKey: '2', globalKey: 'NUM 2', displayName: '2' },
+  Digit3: { browserKey: '3', globalKey: 'NUM 3', displayName: '3' },
+  Digit4: { browserKey: '4', globalKey: 'NUM 4', displayName: '4' },
+  Digit5: { browserKey: '5', globalKey: 'NUM 5', displayName: '5' },
+  Digit6: { browserKey: '6', globalKey: 'NUM 6', displayName: '6' },
+  Digit7: { browserKey: '7', globalKey: 'NUM 7', displayName: '7' },
+  Digit8: { browserKey: '8', globalKey: 'NUM 8', displayName: '8' },
+  Digit9: { browserKey: '9', globalKey: 'NUM 9', displayName: '9' },
+
+  // 특수키
+  ShiftLeft: { browserKey: 'Shift', globalKey: 'LEFT SHIFT', displayName: 'LShift' },
+  ShiftRight: { browserKey: 'Shift', globalKey: 'RIGHT SHIFT', displayName: 'RShift' },
+  ControlLeft: { browserKey: 'Control', globalKey: 'LEFT CTRL', displayName: 'LCtrl' },
+  ControlRight: { browserKey: 'Control', globalKey: 'RIGHT CTRL', displayName: 'RCtrl' },
+  AltLeft: { browserKey: 'Alt', globalKey: 'LEFT ALT', displayName: 'LAlt' },
+  AltRight: { browserKey: 'Alt', globalKey: 'RIGHT ALT', displayName: 'RAlt' },
+  Space: { browserKey: ' ', globalKey: 'SPACE', displayName: 'Space' },
+  Enter: { browserKey: 'Enter', globalKey: 'RETURN', displayName: 'Enter' },
+  Tab: { browserKey: 'Tab', globalKey: 'TAB', displayName: 'Tab' },
+  Backspace: { browserKey: 'Backspace', globalKey: 'BACK', displayName: 'Backspace' },
+  CapsLock: { browserKey: 'CapsLock', globalKey: 'CAPITAL', displayName: 'Caps' },
+
+  // 기능키
+  F1: { browserKey: 'F1', globalKey: 'F1', displayName: 'F1' },
+  F2: { browserKey: 'F2', globalKey: 'F2', displayName: 'F2' },
+  F3: { browserKey: 'F3', globalKey: 'F3', displayName: 'F3' },
+  F4: { browserKey: 'F4', globalKey: 'F4', displayName: 'F4' },
+  F5: { browserKey: 'F5', globalKey: 'F5', displayName: 'F5' },
+  F6: { browserKey: 'F6', globalKey: 'F6', displayName: 'F6' },
+  F7: { browserKey: 'F7', globalKey: 'F7', displayName: 'F7' },
+  F8: { browserKey: 'F8', globalKey: 'F8', displayName: 'F8' },
+  F9: { browserKey: 'F9', globalKey: 'F9', displayName: 'F9' },
+  F10: { browserKey: 'F10', globalKey: 'F10', displayName: 'F10' },
+  F11: { browserKey: 'F11', globalKey: 'F11', displayName: 'F11' },
+  F12: { browserKey: 'F12', globalKey: 'F12', displayName: 'F12' },
+
+  // 방향키
+  ArrowUp: { browserKey: 'ArrowUp', globalKey: 'UP ARROW', displayName: '↑' },
+  ArrowDown: { browserKey: 'ArrowDown', globalKey: 'DOWN ARROW', displayName: '↓' },
+  ArrowLeft: { browserKey: 'ArrowLeft', globalKey: 'LEFT ARROW', displayName: '←' },
+  ArrowRight: { browserKey: 'ArrowRight', globalKey: 'RIGHT ARROW', displayName: '→' },
+
+  // 특수문자
+  Minus: { browserKey: '-', globalKey: 'MINUS', displayName: '-' },
+  Equal: { browserKey: '=', globalKey: 'EQUALS', displayName: '=' },
+  BracketLeft: { browserKey: '[', globalKey: 'SQUARE BRACKET OPEN', displayName: '[' },
+  BracketRight: { browserKey: ']', globalKey: 'SQUARE BRACKET CLOSE', displayName: ']' },
+  Semicolon: { browserKey: ';', globalKey: 'SEMICOLON', displayName: ';' },
+  Quote: { browserKey: "'", globalKey: 'QUOTE', displayName: "'" },
+  Backquote: { browserKey: '`', globalKey: 'BACKTICK', displayName: '`' },
+  Backslash: { browserKey: '\\', globalKey: 'BACK SLASH', displayName: '\\' },
+  Comma: { browserKey: ',', globalKey: 'COMMA', displayName: ',' },
+  Period: { browserKey: '.', globalKey: 'DOT', displayName: '.' },
+  Slash: { browserKey: '/', globalKey: 'FORWARD SLASH', displayName: '/' },
+
+  // 넘패드
+  Numpad0: { browserKey: '0', globalKey: 'NUMPAD 0', displayName: 'Num0' },
+  Numpad1: { browserKey: '1', globalKey: 'NUMPAD 1', displayName: 'Num1' },
+  Numpad2: { browserKey: '2', globalKey: 'NUMPAD 2', displayName: 'Num2' },
+  Numpad3: { browserKey: '3', globalKey: 'NUMPAD 3', displayName: 'Num3' },
+  Numpad4: { browserKey: '4', globalKey: 'NUMPAD 4', displayName: 'Num4' },
+  Numpad5: { browserKey: '5', globalKey: 'NUMPAD 5', displayName: 'Num5' },
+  Numpad6: { browserKey: '6', globalKey: 'NUMPAD 6', displayName: 'Num6' },
+  Numpad7: { browserKey: '7', globalKey: 'NUMPAD 7', displayName: 'Num7' },
+  Numpad8: { browserKey: '8', globalKey: 'NUMPAD 8', displayName: 'Num8' },
+  Numpad9: { browserKey: '9', globalKey: 'NUMPAD 9', displayName: 'Num9' },
+  NumpadMultiply: { browserKey: '*', globalKey: 'NUMPAD MULTIPLY', displayName: 'Num*' },
+  NumpadAdd: { browserKey: '+', globalKey: 'NUMPAD PLUS', displayName: 'Num+' },
+  NumpadSubtract: { browserKey: '-', globalKey: 'NUMPAD MINUS', displayName: 'Num-' },
+  NumpadDecimal: { browserKey: '.', globalKey: 'NUMPAD DOT', displayName: 'Num.' },
+  NumpadDivide: { browserKey: '/', globalKey: 'NUMPAD DIVIDE', displayName: 'Num/' },
+  NumpadEnter: { browserKey: 'Enter', globalKey: 'NUMPAD RETURN', displayName: 'NumEnter' },
+
+  // 기타
+  Escape: { browserKey: 'Escape', globalKey: 'ESCAPE', displayName: 'Esc' },
+  PrintScreen: { browserKey: 'PrintScreen', globalKey: 'PRINT SCREEN', displayName: 'PrtSc' },
+  ScrollLock: { browserKey: 'ScrollLock', globalKey: 'SCROLL LOCK', displayName: 'ScrLk' },
+  Pause: { browserKey: 'Pause', globalKey: 'PAUSE', displayName: 'Pause' },
+  Insert: { browserKey: 'Insert', globalKey: 'INSERT', displayName: 'Ins' },
+  Home: { browserKey: 'Home', globalKey: 'HOME', displayName: 'Home' },
+  PageUp: { browserKey: 'PageUp', globalKey: 'PAGE UP', displayName: 'PgUp' },
+  Delete: { browserKey: 'Delete', globalKey: 'DELETE', displayName: 'Del' },
+  End: { browserKey: 'End', globalKey: 'END', displayName: 'End' },
+  PageDown: { browserKey: 'PageDown', globalKey: 'PAGE DOWN', displayName: 'PgDn' }
+};
+
+export const getKeyInfo = (code, key) => {
+  return keyMaps[code] || {
+    browserKey: key,
+    globalKey: key.toUpperCase(),
+    displayName: key.toUpperCase()
+  };
+};
