25e0421d66d1b07c725eb9dd9f6a0eb9823f30cf
lee-sihun <cnsa201119@gmail.com>
2025-01-11 13:55:04 +0900

    style: 모달 스타일 수정

--- src/renderer/components/KeySettingModal.jsx
+++ src/renderer/components/KeySettingModal.jsx
@@ -25,24 +25,33 @@ export default function KeySettingModal({ keyData, onClose, onSave }) {
   };
 
   return (
-    <div className="fixed top-[41px] left-[1px] flex items-center justify-center w-[896px] h-[451px] bg-[#000000] bg-opacity-[0.31] backdrop-blur-[37.5px] rounded-b-[6px]">
-      <div className="flex flex-col items-center justify-center p-[25px] bg-[#1C1E25] border border-[#3B4049] rounded-[6px]">
-        <button 
-          onClick={() => setIsListening(true)}
-          className="flex items-center h-[25px] px-[9px] bg-[#101216] rounded-[6px] border-[0.5px] border-[#3B4049] text-[#989BA6] text-[13.5px] font-extrabold"
-        >
-          {isListening ? "Press any key..." : displayKey || "Click to set key"}
-        </button>
-        <div className="flex mt-2">
+    <div 
+      className="fixed top-[41px] left-[1px] flex items-center justify-center w-[896px] h-[451px] bg-[#000000] bg-opacity-[0.31] backdrop-blur-[37.5px] rounded-b-[6px]"
+      onClick={onClose}
+    >
+      <div 
+        className="flex flex-col items-center justify-center w-[334.5px] p-[25px] bg-[#1C1E25] border border-[#3B4049] rounded-[6px]"
+        onClick={(e) => e.stopPropagation()}
+      >
+        <div className='flex justify-between w-full align-center'>
+          <h2 className="text-white text-[13.5px] font-extrabold leading-[24.5px]">키 매핑</h2>
+          <button 
+            onClick={() => setIsListening(true)}
+            className="flex items-center h-[24.6px] px-[9px] bg-[#101216] rounded-[6px] border-[0.5px] border-[#3B4049] text-[#989BA6] text-[13.5px] font-extrabold"
+          >
+            {isListening ? "Press any key" : displayKey || "Click to set key"}
+          </button>
+        </div>
+        <div className="flex w-full justify-between h-[31.5px] mt-[30.25px] gap-[15px]">
           <button 
             onClick={handleSubmit}
-            className="px-4 py-2 bg-[#101216] rounded-[6px] border-[0.5px] border-[#3B4049] text-[#989BA6] text-[13.5px] font-extrabold"
+            className="flex-1 bg-[#101216] rounded-[6px] border-[0.5px] border-[#3B4049] text-[#FFFFFF] text-[15px] font-semibold"
           >
             저장
           </button>
           <button 
             onClick={onClose}
-            className="px-4 py-2 bg-[#101216] rounded-[6px] border-[0.5px] border-[#3B4049] text-[#989BA6] text-[13.5px] font-extrabold"
+            className="flex-1 bg-[#101216] rounded-[6px] border-[0.5px] border-[#3B4049] text-[#FFFFFF] text-[15px] font-semibold"
           >
             취소
           </button>
