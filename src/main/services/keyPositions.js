08d2c0bd55a3502dc159ef0ec7d62011b381123e
lee-sihun <cnsa201119@gmail.com>
2025-01-19 19:51:11 +0900

    feat: 기본 키 간격 수정

--- src/main/services/keyPositions.js
+++ src/main/services/keyPositions.js
@@ -3,43 +3,43 @@ const store = new Store();
 
 const DEFAULT_POSITIONS = {
   "4key": [
-    { dx: 280, dy: 130, width: 60, activeImage: '', inactiveImage: '', count: 0 },
+    { dx: 290, dy: 130, width: 60, activeImage: '', inactiveImage: '', count: 0 },
     { dx: 360, dy: 130, width: 60, activeImage: '', inactiveImage: '', count: 0 },
-    { dx: 440, dy: 130, width: 60, activeImage: '', inactiveImage: '', count: 0 },
-    { dx: 520, dy: 130, width: 60, activeImage: '', inactiveImage: '', count: 0 },
-    { dx: 140, dy: 130, width: 120, activeImage: '', inactiveImage: '', count: 0 },
-    { dx: 600, dy: 130, width: 120, activeImage: '', inactiveImage: '', count: 0 },
+    { dx: 430, dy: 130, width: 60, activeImage: '', inactiveImage: '', count: 0 },
+    { dx: 500, dy: 130, width: 60, activeImage: '', inactiveImage: '', count: 0 },
+    { dx: 160, dy: 130, width: 120, activeImage: '', inactiveImage: '', count: 0 },
+    { dx: 570, dy: 130, width: 120, activeImage: '', inactiveImage: '', count: 0 },
   ],
   "5key": [
-    { dx: 240, dy: 130, width: 60, activeImage: '', inactiveImage: '', count: 0 },
-    { dx: 320, dy: 130, width: 60, activeImage: '', inactiveImage: '', count: 0 },
+    { dx: 260, dy: 130, width: 60, activeImage: '', inactiveImage: '', count: 0 },
+    { dx: 330, dy: 130, width: 60, activeImage: '', inactiveImage: '', count: 0 },
     { dx: 400, dy: 130, width: 60, activeImage: '', inactiveImage: '', count: 0 },
-    { dx: 480, dy: 130, width: 60, activeImage: '', inactiveImage: '', count: 0 },
-    { dx: 560, dy: 130, width: 60, activeImage: '', inactiveImage: '', count: 0 },
-    { dx: 100, dy: 130, width: 120, activeImage: '', inactiveImage: '', count: 0 },
-    { dx: 640, dy: 130, width: 120, activeImage: '', inactiveImage: '', count: 0 }
+    { dx: 470, dy: 130, width: 60, activeImage: '', inactiveImage: '', count: 0 },
+    { dx: 540, dy: 130, width: 60, activeImage: '', inactiveImage: '', count: 0 },
+    { dx: 130, dy: 130, width: 120, activeImage: '', inactiveImage: '', count: 0 },
+    { dx: 610, dy: 130, width: 120, activeImage: '', inactiveImage: '', count: 0 }
   ],
-"6key": [
-    { dx: 200, dy: 130, width: 60, activeImage: '', inactiveImage: '', count: 0 },
-    { dx: 280, dy: 130, width: 60, activeImage: '', inactiveImage: '', count: 0 },
-    { dx: 360, dy: 130, width: 60, activeImage: '', inactiveImage: '', count: 0 },
+  "6key": [
+    { dx: 230, dy: 130, width: 60, activeImage: '', inactiveImage: '', count: 0 },
+    { dx: 300, dy: 130, width: 60, activeImage: '', inactiveImage: '', count: 0 },
+    { dx: 370, dy: 130, width: 60, activeImage: '', inactiveImage: '', count: 0 },
     { dx: 440, dy: 130, width: 60, activeImage: '', inactiveImage: '', count: 0 },
-    { dx: 520, dy: 130, width: 60, activeImage: '', inactiveImage: '', count: 0 },
-    { dx: 600, dy: 130, width: 60, activeImage: '', inactiveImage: '', count: 0 },
-    { dx: 60, dy: 130, width: 120, activeImage: '', inactiveImage: '', count: 0 },
-    { dx: 680, dy: 130, width: 120, activeImage: '', inactiveImage: '', count: 0 }
+    { dx: 510, dy: 130, width: 60, activeImage: '', inactiveImage: '', count: 0 },
+    { dx: 580, dy: 130, width: 60, activeImage: '', inactiveImage: '', count: 0 },
+    { dx: 100, dy: 130, width: 120, activeImage: '', inactiveImage: '', count: 0 },
+    { dx: 650, dy: 130, width: 120, activeImage: '', inactiveImage: '', count: 0 }
   ],
   "8key": [
-    { dx: 200, dy: 90, width: 60, activeImage: '', inactiveImage: '', count: 0 },
-    { dx: 280, dy: 90, width: 60, activeImage: '', inactiveImage: '', count: 0 },
-    { dx: 360, dy: 90, width: 60, activeImage: '', inactiveImage: '', count: 0 },
+    { dx: 230, dy: 90, width: 60, activeImage: '', inactiveImage: '', count: 0 },
+    { dx: 300, dy: 90, width: 60, activeImage: '', inactiveImage: '', count: 0 },
+    { dx: 370, dy: 90, width: 60, activeImage: '', inactiveImage: '', count: 0 },
     { dx: 440, dy: 90, width: 60, activeImage: '', inactiveImage: '', count: 0 },
-    { dx: 520, dy: 90, width: 60, activeImage: '', inactiveImage: '', count: 0 },
-    { dx: 600, dy: 90, width: 60, activeImage: '', inactiveImage: '', count: 0 },
-    { dx: 60, dy: 90, width: 120, activeImage: '', inactiveImage: '', count: 0 },
-    { dx: 680, dy: 90, width: 120, activeImage: '', inactiveImage: '', count: 0 },
-    { dx: 250, dy: 170, width: 120, activeImage: '', inactiveImage: '', count: 0 },
-    { dx: 490, dy: 170, width: 120, activeImage: '', inactiveImage: '', count: 0 }
+    { dx: 510, dy: 90, width: 60, activeImage: '', inactiveImage: '', count: 0 },
+    { dx: 580, dy: 90, width: 60, activeImage: '', inactiveImage: '', count: 0 },
+    { dx: 100, dy: 90, width: 120, activeImage: '', inactiveImage: '', count: 0 },
+    { dx: 650, dy: 90, width: 120, activeImage: '', inactiveImage: '', count: 0 },
+    { dx: 270, dy: 160, width: 120, activeImage: '', inactiveImage: '', count: 0 },
+    { dx: 480, dy: 160, width: 120, activeImage: '', inactiveImage: '', count: 0 }
   ]
 }
 
