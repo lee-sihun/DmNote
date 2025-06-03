1104c92a155492c650e6cb10ce796fa5eabb062d
lee-sihun <cnsa201119@gmail.com>
2025-06-03 10:54:43 +0900

    feat: 넘패드 키 매핑 및 통합 처리 추가

--- src/main/services/keyboardListener.js
+++ src/main/services/keyboardListener.js
@@ -8,6 +8,28 @@ const { loadKeys, saveKeys } = require('./keyMappings')
 // scanCode: e.scanCode,   // 스캔 코드
 // modifiers: e.modifiers  // 수정자 키
 
+// 넘패드 키 매핑 테이블 (같은 물리적 키)
+const NUMPAD_KEY_GROUPS = {
+  'NUMPAD 0': ['NUMPAD 0', 'INS'],
+  'NUMPAD 1': ['NUMPAD 1', 'END'],
+  'NUMPAD 2': ['NUMPAD 2', 'DOWN ARROW'],
+  'NUMPAD 3': ['NUMPAD 3', 'PAGE DOWN'],
+  'NUMPAD 4': ['NUMPAD 4', 'LEFT ARROW'],
+  'NUMPAD 5': ['NUMPAD 5', 'NUMPAD CLEAR'],
+  'NUMPAD 6': ['NUMPAD 6', 'RIGHT ARROW'],
+  'NUMPAD 7': ['NUMPAD 7', 'HOME'],
+  'NUMPAD 8': ['NUMPAD 8', 'UP ARROW'],
+  'NUMPAD 9': ['NUMPAD 9', 'PAGE UP'],
+};
+
+// 키 -> 대표키 매핑 테이블 
+const KEY_TO_REPRESENTATIVE = {};
+Object.entries(NUMPAD_KEY_GROUPS).forEach(([representative, keys]) => {
+  keys.forEach(key => {
+    KEY_TO_REPRESENTATIVE[key] = representative;
+  });
+});
+
 class KeyboardService {
   constructor() {
     this.listener = new GlobalKeyboardListener();
@@ -48,6 +70,12 @@ class KeyboardService {
     let key = e.name || e.vKey.toString();
     const state = e.state;
 
+    // 넘패드 키 통합 처리
+    const representativeKey = KEY_TO_REPRESENTATIVE[key];
+    if (representativeKey) {
+      key = representativeKey;
+    }
+
     // 배열 순차 탐색에서 해시 테이블 기반 조회로 개선 
     if (!this.validKeySet.has(key)) {
       return;
@@ -57,7 +85,21 @@ class KeyboardService {
   }
 
   updateValidKeySet() {
-    this.validKeySet = new Set(this.keys[this.currentMode] || []);
+    const currentKeys = this.keys[this.currentMode] || [];
+    this.validKeySet = new Set();
+
+    currentKeys.forEach(key => {
+      // 설정된 키 추가
+      this.validKeySet.add(key);
+
+      // 넘패드 키 그룹 추가
+      const keyGroup = NUMPAD_KEY_GROUPS[key];
+      if (keyGroup) {
+        keyGroup.forEach(variantKey => {
+          this.validKeySet.add(variantKey);
+        });
+      }
+    });
   }
 
   updateKeyMapping(keys) {
